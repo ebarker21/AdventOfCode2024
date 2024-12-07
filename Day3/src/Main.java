57c51c8c94ed5b71c62cfe158d0c2495d7063f22
Eric <ericbarker21@gmail.com>
2024-12-07 02:54:12 -0500

    Adds Day3 part 2, see ln 69

--- Day3/src/Main.java
+++ Day3/src/Main.java
@@ -10,6 +10,8 @@ public class Main {
         String inputString = inputToString(input);
         int total = mulParser(inputString);
         System.out.println("total: "+total);
+        total = mulParser(removeDonts(inputString));
+        System.out.println("total without don'ts: "+total);
     }
 
     private static int mulParser(String input){
@@ -64,9 +66,9 @@ public class Main {
         }
         return total;
     }
-
+//BUG the outputted string includes do_not_mul() instead of don't()
     private static String inputToString(File input){
-        Scanner sc = null;
+        Scanner sc;
         try {
             sc = new Scanner(input);
         } catch (FileNotFoundException e) {
@@ -77,5 +79,54 @@ public class Main {
             output += sc.nextLine();
         }
         return output;
+    }
+
+    private static String removeDonts(String input){
+        int begin = 0;
+        boolean good = true;
+        String doString = "";
+        for(int i = 0; i < input.length(); i++){
+            if(good){
+                if(input.charAt(i) == 'd'){
+                    i++;
+                    if(input.charAt(i) == 'o'){
+                        i++;
+                        if(input.charAt(i) == 'n'){
+                            i++;
+                            if(input.charAt(i) == '\''){
+                                i++;
+                                if(input.charAt(i) == 't'){
+                                    i++;
+                                    if(input.charAt(i) == '('){
+                                        i++;
+                                        if(input.charAt(i) == ')'){
+                                            i++;
+                                            doString += input.substring(begin, i);
+                                            good = false;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }else{
+                if(input.charAt(i) == 'd') {
+                    i++;
+                    if (input.charAt(i) == 'o') {
+                        i++;
+                        if (input.charAt(i) == '(') {
+                            i++;
+                            if (input.charAt(i) == ')') {
+                                begin = i;
+                                good = true;
+                                i++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return doString;
     }
 }
